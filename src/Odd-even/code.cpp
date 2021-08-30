f0836f233af293d4110b2aedaecf5d76763fbd44
hemang-joshi <73999607+hemang-joshi@users.noreply.github.com>
2021-08-30 09:58:40 +0530

    Initial Commit
    Check ReadMe.md file to know about the question

---
+++ src/Odd-even/code.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+
+int main()
+{
+	int number = 0;
+	cout << "Enter integer number\n";
+	cin >> number;
+	if (number % 2 == 0) {
+		cout << "The value " << number << " is an even number.\n";
+	}
+	else {
+		cout << "The value " << number << " is an odd number.\n";
+	}
+	return 0;
+}
